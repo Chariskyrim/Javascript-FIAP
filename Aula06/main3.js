bf162a54f5d3db39d2bad7b2afa8ee30b885fb7d
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-02 22:30:57 -0300

    Aula06

---
+++ Aula06/main3.js
@@ -0,0 +1,43 @@
+/*function caixa(texto, tam) {
+    console.log("---")
+    console.log(texto)
+    console.log("---")
+}
+
+let i = 0
+while (i < 10) {
+    i = i + 2
+}/*
+
+
+/*function tracos (qtde) {
+    let texto = ""
+    for (let i = 0 ; i < qtde ; i++){
+        texto = texto + "-"
+    }
+    console.log(texto)
+}
+
+function caixa (texto, qtde) {
+    tracos (qtde)
+    console.log(texto)
+    tracos(qtde)
+}
+
+caixa("Ana", 3)
+caixa("Bia", 3)
+caixa("João", 4)
+*/
+
+function areaRetangulo( larg, alt) {
+    let area = larg * alt
+    return area
+}
+
+let x = 5
+
+let soma = function (x, y) {
+
+    return  x + y
+    
+}
